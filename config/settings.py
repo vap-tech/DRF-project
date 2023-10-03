a2ed20bb487544cb4146fcab5abf7ab772c1b8b1
kot <bafomet2016vitt@gmail.com>
2023-10-04 02:58:17 +0300

    add payment

--- config/settings.py
+++ config/settings.py
@@ -116,13 +116,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
