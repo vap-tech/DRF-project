52e91dc0e5bc7d5ecf513d3251a94e350d3cd3dd
kot <bafomet2016vitt@gmail.com>
2023-10-11 02:50:17 +0300

    add owner

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,7 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
 }
 
 SIMPLE_JWT = {
