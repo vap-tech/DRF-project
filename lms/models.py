c22f81e2fa32eaf80909ef77dc7fea2a572cbcda
kot <bafomet2016vitt@gmail.com>
2023-10-11 02:52:14 +0300

    add owner

--- lms/models.py
+++ lms/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     preview = models.ImageField(upload_to='course/', verbose_name='превью', **NULLABLE)
     description = models.TextField(verbose_name='описание')
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE)
+    owner = models.ForeignKey(User, verbose_name='владелец', on_delete=models.CASCADE, **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
@@ -25,7 +25,7 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='описание', **NULLABLE)
     video_url = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE)
+    owner = models.ForeignKey(User, verbose_name='владелец', on_delete=models.CASCADE, **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
